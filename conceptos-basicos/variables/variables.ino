2e070668a244dbdbb851ccffdf30dc01b358c96f
Matthias Clein <m.cleinespinoza@gmail.com>
2022-08-30 15:26:12 -0400

    add conceptos-basicos

---
+++ conceptos-basicos/variables/variables.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // Código para configurar la placa.
+  // Se ejecuta una sola vez, luego de que la placa se energiza.
+}
+
+void loop() {
+  int edad = 20; // Variable numérica entera.
+  float pi = 3.14; // Variable numérica decimal/real.
+  char letra = 'A'; // Variable tipo carácter.
+  bool valor = true;  // Variable booleana.
+}
